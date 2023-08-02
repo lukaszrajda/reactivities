a8b795c5da876d82cb72025805faf49ee7d58044
Lukasz Rajda <lukasz.rajda@fortedigital.no>
2023-08-02 07:11:57 +0200

    Complete CRUD operations on client and persist changes with using api

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,10 +14,11 @@ interface Props {
   deleteActivity: (id: string) => void;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity,
-  editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+  editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
@@ -25,12 +26,13 @@ export default function ActivityDashboard({ activities, selectedActivity, select
           activities={activities}
           selectActivity={selectActivity}
           deleteActivity={deleteActivity}
+          submitting={submitting}
         />
       </Grid.Column>
       <Grid.Column width='6'>
         {selectedActivity && !editMode &&
           <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm} />}
-        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} submitting={submitting} />}
       </Grid.Column>
     </Grid>
   )
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props {
   closeForm: () => void;
   activity: Activity | undefined;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
-export default function ActivityForm({ closeForm, activity: selectedActivity, createOrEdit }: Props) {
+export default function ActivityForm({ closeForm, activity: selectedActivity, createOrEdit, submitting }: Props) {
   const initialState = selectedActivity ?? {
     id: '',
     title: '',
@@ -26,8 +27,8 @@ export default function ActivityForm({ closeForm, activity: selectedActivity, cr
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    const {name, value} = event.target;
-    setActivity({...activity, [name]: value});
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value });
   }
 
   return (
@@ -36,10 +37,10 @@ export default function ActivityForm({ closeForm, activity: selectedActivity, cr
         <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
         <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
         <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
-        <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+        <Form.Input placeholder='Date' type='date' value={activity.date} name='date' onChange={handleInputChange} />
         <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
         <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-        <Button floated='right' positive type='submit' content='Submit' />
+        <Button floated='right' positive type='submit' content='Submit' loading={submitting} />
         <Button floated='right' type='button' content='Cancel' onClick={closeForm} />
       </Form>
     </Segment>
