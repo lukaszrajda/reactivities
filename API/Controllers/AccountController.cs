3fe6b12f4a2165b2a5f477845b12ea0a73947aed
Lukasz Rajda <lukasz.rajda@fortedigital.no>
2023-08-14 09:42:16 +0200

    Add attend feature, secure edit and delete endpoint, add db relations

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -86,7 +86,7 @@ namespace API.Controllers
         DisplayName = user.DisplayName,
         Image = null,
         Token = _tokenService.CreateToken(user),
-        UserName = user.UserName
+        Username = user.UserName
       };
     }
   }
